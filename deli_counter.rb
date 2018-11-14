a1bfcde81e906ea46c2da7a4523d36e1155fa9d6
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 05:33:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(customers) 
+  if costumers.empty?
+    "The line is currently empty."
+  else
+    nil
+  end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "#{name} | #{line.length+1}"
+end
+
+def now_serving(line)
+
+  if line.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "now serving: #{line.first}!"
+    line.shift
+  end
+end
