9afb1c55f08f2406fbbefae3907a7fb4357f41e6
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 05:53:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,10 @@ def line(customers)
   if customers.empty?
     puts "The line is currently empty."
   else
-
     print "The line is currently:"
     customers.each_with_index do |customer, number|
       print " #{number+1}. #{customer}"
     end
-    print "\n"
   end
 end
 
